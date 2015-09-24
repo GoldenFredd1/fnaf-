dda4980ea45bfaf18365776264fa960af5ecf95e
midnightfreddie <jim@midnightfreddie.com>
2015-09-24 00:14:37 -0500

    Added kill detection, stats and display

--- controllers.js
+++ controllers.js
@@ -7,18 +7,21 @@ securityGuardGame.controller('gameState', ['$scope', '$interval', '$http', '$sce
 
     $http.get("views.json")
       .success(function(response) {
-        $scope.views = response
+        $scope.views = response;
       });
     // Array of objects describing enemy attributes and behaviors
     $http.get("enemies.json")
       .success(function(response) {
-        $scope.enemies = response
+        $scope.enemies = response;
+        $scope.enemies.forEach( function(enemy) { enemy['kills'] = 0; } );
       });
 
   // Reassign enemy location bgased on current location and Markov chain
   $scope.move = function(enemy) {
+    var oldlocation = enemy.location;
     moves = enemy.movesMarkovChain[enemy.location];
     enemy.location = moves[Math.floor(Math.random()*moves.length)];
+    if (enemy.location == 999 && oldlocation != 999) enemy.kills++;
   };
 
   // Actions to perform each game tick: increment time, move each enemy
